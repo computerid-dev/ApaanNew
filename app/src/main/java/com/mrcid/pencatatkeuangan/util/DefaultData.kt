9dfa96fcc4d0fc1a91f901965a8a2fb591360865
computerid-dev <nugrohokelyn@gmail.com>
2026-07-22 13:49:11 +0700

    Upload app/src/main/java/com/mrcid/pencatatkeuangan/util/DefaultData.kt

---
+++ app/src/main/java/com/mrcid/pencatatkeuangan/util/DefaultData.kt
@@ -0,0 +1,41 @@
+package com.mrcid.pencatatkeuangan.util
+
+/**
+ * Daftar kategori barang bawaan aplikasi.
+ * Otomatis dimasukkan ke database saat aplikasi pertama kali dibuka.
+ */
+object DefaultData {
+
+    val DAFTAR_KATEGORI = listOf(
+        "Elektronik",
+        "Handphone & Tablet",
+        "Komputer & Laptop",
+        "Peralatan Dapur",
+        "Alat Rumah Tangga",
+        "Perabotan & Furniture",
+        "Bahan Bangunan",
+        "Peralatan Pertukangan",
+        "Makanan & Minuman",
+        "Sembako",
+        "Pakaian & Fashion",
+        "Sepatu & Sandal",
+        "Tas & Dompet",
+        "Aksesoris",
+        "Perhiasan",
+        "Kesehatan & Kecantikan",
+        "Perlengkapan Bayi & Anak",
+        "Mainan",
+        "Tanaman & Berkebun",
+        "Hewan Peliharaan",
+        "Otomotif & Sparepart",
+        "Alat Tulis & Kantor",
+        "Buku & Media",
+        "Olahraga & Outdoor",
+        "Musik & Alat Musik",
+        "Hobi & Koleksi",
+        "Kerajinan Tangan",
+        "Produk Digital",
+        "Souvenir & Hadiah",
+        "Lainnya"
+    )
+}
